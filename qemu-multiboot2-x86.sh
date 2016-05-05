7cc704c3e529c17c187dc1d50033ef70b9190bcf
pedro.lamarao <pedro.lamarao@localhost>
2016-05-05 02:09:16 -0300

    Remove src-multiboot2-x86 example program.

--- qemu-multiboot2-x86.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -f "bin-multiboot2-x86/aasgard" image/iso/boot/kernel
-
-rm image/iso.img
-
-grub2-mkrescue -o image/iso.img image/iso
-
-/usr/bin/qemu-system-i386 -m 512 -cdrom image/iso.img -no-kvm -s
