3c0fb7f3a82874ce462e454941af4a0bd2e31453
pedro.lamarao <pedro.lamarao@localhost>
2016-05-05 02:10:10 -0300

    Remove old multiboot2 files.
     * multiboot2.h: remove.
     * multiboot2_assert.cpp: remove.

--- multiboot2/multiboot2_assert.cpp
+++
@@ -1,8 +0,0 @@
-#include "multiboot2.h"
-
-
-static_assert(__is_standard_layout(multiboot2::request_header), "multiboot2::request_header is not standard layout");
-
-static_assert(__is_standard_layout(multiboot2::information_header), "multiboot2::information_header is not standard layout");
-
-static_assert(__is_standard_layout(multiboot2::information_entry_base), "multiboot2::information_entry_base is not standard layout");
