6986e793210bcc3832a501f1d8fc7ed6ab364e17
pedro.lamarao <pedro.lamarao@rj-ws-pedro-20160503>
2016-05-12 19:33:45 -0300

    Define interrupt handlers, enable PIC.
     * start.s: define specific interrupt and exception handlers.
     * main.cpp: add generic interrupt and exception handlers;
     in main, define an IDT with specific handlers and
     initialize PIC with inline assembly.
     * gdbinit: break on handlers.
     * TODO.md: update.

--- xxx--multiboot2-x86--io/TODO.md
+++ xxx--multiboot2-x86--io/TODO.md
@@ -2,10 +2,10 @@
 
 ## Doing
 
-* Satisfy requirement (1) by storing the GDT addressable by `lgdt`
-* Guarantee (1)
-* Satisfy requirement (2) by storing the IDT addressable by `lidt`
-* Guarantee (2)
-* Handle an `int $0x80`
+* Initialize PIC with C++
 
 ## Done
+
+* Define interrupt/exception in C++
+* Initialize PIC with inline assembly
+* Trap interrupt handling in pure assembly
--- xxx--multiboot2-x86--io/main.cpp
+++ xxx--multiboot2-x86--io/main.cpp
@@ -16,7 +16,9 @@
 
 namespace
 {
-  using namespace multiboot2;
+  using multiboot2::architecture_type;
+  using multiboot2::end_request;
+  using multiboot2::header_prologue;
 
   struct
   {
@@ -28,57 +30,14 @@ namespace
     { architecture_type::x86, sizeof(multiboot2_header), },
     { },
   };
-
-  multiboot2::end_information const * end_item = nullptr;
-  multiboot2::command_line_information const  * command_line_item = nullptr;
-  multiboot2::loader_name_information const  * loader_name_item = nullptr;
-  multiboot2::modules_information const  * modules_item = nullptr;
-  multiboot2::basic_memory_information const  * basic_memory_item = nullptr;
-  multiboot2::boot_device_information const  * boot_device_item = nullptr;
-  multiboot2::memory_map_information const  * memory_map_item = nullptr;
-  multiboot2::vbe_information const  * vbe_item = nullptr;
-  multiboot2::framebuffer_information const  * framebuffer_item = nullptr;
-  multiboot2::elf_symbols_information const  * elf_symbols_item = nullptr;
-  multiboot2::apm_information const  * apm_item = nullptr;
-  multiboot2::efi32_information const  * efi32_item = nullptr;
-  multiboot2::efi64_information const  * efi64_item = nullptr;
-  multiboot2::smbios_information const  * smbios_item = nullptr;
-  multiboot2::acpi_information const  * acpi_old_item = nullptr;
-  multiboot2::acpi_information const  * acpi_new_item = nullptr;
-  multiboot2::network_information const  * network_item = nullptr;
-
-  void parse_multiboot2_information ( multiboot2::information_list & list )
-  {
-	  for (auto i = begin(list), j = end(list); i != j; i = next(i))
-	  {
-	    using type = multiboot2::information_type;
-	    switch (i->type)
-	    {
-	      case type::end: end_item = reinterpret_cast<end_information*>(i); break;
-	      case type::command_line: command_line_item = reinterpret_cast<command_line_information*>(i); break;
-	      case type::loader_name: loader_name_item = reinterpret_cast<loader_name_information*>(i); break;
-	      case type::modules: modules_item = reinterpret_cast<modules_information*>(i); break;
-	      case type::basic_memory: basic_memory_item = reinterpret_cast<basic_memory_information*>(i); break;
-	      case type::boot_device: boot_device_item = reinterpret_cast<boot_device_information*>(i); break;
-	      case type::memory_map: memory_map_item = reinterpret_cast<memory_map_information*>(i); break;
-	      case type::vbe: vbe_item = reinterpret_cast<vbe_information*>(i); break;
-	      case type::framebuffer: framebuffer_item = reinterpret_cast<framebuffer_information*>(i); break;
-	      case type::elf_symbols: elf_symbols_item = reinterpret_cast<elf_symbols_information*>(i); break;
-	      case type::apm: apm_item = reinterpret_cast<apm_information*>(i); break;
-	      case type::efi32: efi32_item = reinterpret_cast<efi32_information*>(i); break;
-	      case type::efi64: efi64_item = reinterpret_cast<efi64_information*>(i); break;
-	      case type::smbios: smbios_item = reinterpret_cast<smbios_information*>(i); break;
-	      case type::acpi_old: acpi_old_item = reinterpret_cast<acpi_information*>(i); break;
-	      case type::acpi_new: acpi_new_item = reinterpret_cast<acpi_information*>(i); break;
-	      case type::network: network_item = reinterpret_cast<network_information*>(i); break;
-	    }
-	  }
-  }
 }
 
 namespace
 {
-  using namespace x86;
+  using x86::segment_descriptor;
+  using x86::code_segment_access;
+  using x86::data_segment_access;
+  using x86::segment_granularity;
 
   constexpr segment_descriptor global_descriptor_table [5] __attribute__((aligned(8), section(".gdt"))) =
   {
@@ -88,28 +47,65 @@ namespace
     { 0, 0xFFFFFFFF, code_segment_access(true, false, 3), segment_granularity(false, true, true) },
     { 0, 0xFFFFFFFF, data_segment_access(true, false, 3), segment_granularity(false, true, true) },
   };
-
-  using gate_descriptor = x86::interrupt_gate_descriptor;
-
-  gate_descriptor interrupt_descriptor_table [256] __attribute__((aligned(8), section(".idt"))) =
-  { };
-
-  extern "C"
-  void __interrupt_service_routine ();
-
-  x86::port<std::uint8_t> idle { 0x80 };
 }
 
-namespace
-{
-  struct pic
-  {
-    x86::port<std::uint8_t> command;
-    x86::port<std::uint8_t> data;
-  };
+extern "C" void __interrupt_handler ();
 
-  pic master_pic { 0x20, 0x21 };
-  pic slave_pic  { 0xA0, 0xA1 };
+extern "C" void __00_exception_handler ();
+extern "C" void __01_exception_handler ();
+extern "C" void __02_exception_handler ();
+extern "C" void __03_exception_handler ();
+extern "C" void __04_exception_handler ();
+extern "C" void __05_exception_handler ();
+extern "C" void __06_exception_handler ();
+extern "C" void __07_exception_handler ();
+extern "C" void __08_exception_handler ();
+extern "C" void __09_exception_handler ();
+extern "C" void __0A_exception_handler ();
+extern "C" void __0B_exception_handler ();
+extern "C" void __0C_exception_handler ();
+extern "C" void __0D_exception_handler ();
+extern "C" void __0E_exception_handler ();
+extern "C" void __0F_exception_handler ();
+extern "C" void __10_exception_handler ();
+extern "C" void __11_exception_handler ();
+extern "C" void __12_exception_handler ();
+extern "C" void __13_exception_handler ();
+extern "C" void __14_exception_handler ();
+
+extern "C" void __20_interrupt_handler ();
+extern "C" void __21_interrupt_handler ();
+extern "C" void __22_interrupt_handler ();
+extern "C" void __23_interrupt_handler ();
+extern "C" void __24_interrupt_handler ();
+extern "C" void __25_interrupt_handler ();
+extern "C" void __26_interrupt_handler ();
+extern "C" void __27_interrupt_handler ();
+extern "C" void __28_interrupt_handler ();
+extern "C" void __29_interrupt_handler ();
+extern "C" void __2A_interrupt_handler ();
+extern "C" void __2B_interrupt_handler ();
+extern "C" void __2C_interrupt_handler ();
+extern "C" void __2D_interrupt_handler ();
+extern "C" void __2E_interrupt_handler ();
+extern "C" void __2F_interrupt_handler ();
+
+void io_wait ()
+{
+  asm volatile ("jmp 1f");
+  asm volatile ("1:");
+  asm volatile ("jmp 2f");
+  asm volatile ("2:");
+}
+
+extern "C" void exception (std::uint32_t level, std::uint32_t error)
+{
+  return;
+}
+
+extern "C" void interrupt (std::uint32_t level)
+{
+  return;
 }
 
 //! Multiboot2 client entry
@@ -123,8 +119,6 @@ void __attribute__((fastcall)) main ( std::uint32_t magic, multiboot2::informati
 
   if (magic != multiboot2::information_magic) return;
 
-  parse_multiboot2_information(mbi);
-
   // load GDT
 
   x86::load_global_descriptor_table(global_descriptor_table);
@@ -133,29 +127,97 @@ void __attribute__((fastcall)) main ( std::uint32_t magic, multiboot2::informati
 
   // load IDT
 
-  for (auto i = 0U, j = 256U; i != j; ++i)
+  using x86::interrupt_gate_access;
+  using x86::interrupt_gate_descriptor;
+
+  interrupt_gate_descriptor idt [] =
   {
-    interrupt_descriptor_table[i] = { 0x8, __interrupt_service_routine, interrupt_gate_access(true, 0) };
-  }
+    // 0x00
+    { 0x8, __00_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __01_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __02_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __03_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __04_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __05_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __06_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __07_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __08_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __09_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __0A_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __0B_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __0C_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __0D_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __0E_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __0F_exception_handler, interrupt_gate_access(true, 0) },
+    // 0x10
+    { 0x8, __10_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __11_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __12_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __13_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __14_exception_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __interrupt_handler, interrupt_gate_access(true, 0) },
+    // 0x20
+    { 0x8, __20_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __21_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __22_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __23_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __24_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __25_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __26_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __27_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __28_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __29_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __2A_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __2B_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __2C_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __2D_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __2E_interrupt_handler, interrupt_gate_access(true, 0) },
+    { 0x8, __2F_interrupt_handler, interrupt_gate_access(true, 0) },
+  };
 
-  x86::load_interrupt_descriptor_table(interrupt_descriptor_table, 256);
-
-  asm volatile ( "sti" );
+  x86::load_interrupt_descriptor_table(idt);
 
   // initialize PIC
 
-  master_pic.data.write(pc::icw1(true, false, false));
-  slave_pic.data.write(pc::icw1(true, false, false));
-  idle.write(0);
-  master_pic.data.write(pc::icw2(0x20));
-  slave_pic.data.write(pc::icw2(0x28));
-  idle.write(0);
-  master_pic.data.write(pc::icw3(0b00000100));
-  slave_pic.data.write(pc::icw3(2));
-  idle.write(0);
-  master_pic.data.write(pc::icw4(true, false, false, false, false));
-  slave_pic.data.write(pc::icw4(true, false, false, false, false));
-  idle.write(0);
+  // ICW1
+  asm volatile ("mov $0x11, %al");
+  asm volatile ("outb %al, $0x20");
+  asm volatile ("outb %al, $0xA0");
+  io_wait();
+  // ICW2
+  asm volatile ("mov $0x20, %al");
+  asm volatile ("outb %al, $0x21");
+  asm volatile ("mov $0x28, %al");
+  asm volatile ("outb %al, $0xA1");
+  io_wait();
+  // ICW3
+  asm volatile ("mov $0x04, %al");
+  asm volatile ("outb %al, $0x21");
+  asm volatile ("mov $0x02, %al");
+  asm volatile ("outb %al, $0xA1");
+  io_wait();
+  // ICW4
+  asm volatile ("mov $0x01, %al");
+  asm volatile ("outb %al, $0x21");
+  asm volatile ("outb %al, $0xA1");
+  // OCW2
+  asm volatile ("mov $0x00, %al");
+  asm volatile ("outb %al, $0x21");
+  asm volatile ("outb %al, $0xA1");
+
+  // enable interrupts
+
+  asm volatile ( "sti" );
 
   return;
 }
