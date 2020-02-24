79336dbbb4fd940f70ef3a0b459d718e1003d96a
Pedro Lamarão <pedro.lamarao@gmail.com>
2020-02-24 14:29:35 -0300

    Debian requires 'flex'

--- tools/debian.sh
+++ tools/debian.sh
@@ -5,5 +5,5 @@ apt-get install  bzip2 wget xz-utils
 if [ $? -ne 0 ]; then exit $?; fi
 
 # build
-apt-get install  bison file gcc g++ libexpat1-dev make texinfo
+apt-get install  bison file flex gcc g++ libexpat1-dev make texinfo
 if [ $? -ne 0 ]; then exit $?; fi
