d56d3045b19c977493d107d137b343c30e7f9d22
Pedro Lamarão <pedro.lamarao@gmail.com>
2020-02-24 22:48:32 -0300

    Set `temporaryDir` for command style `system`

--- gradle-qemu/src/main/java/aasgard/gradle/qemu/QemuExtension.java
+++ gradle-qemu/src/main/java/aasgard/gradle/qemu/QemuExtension.java
@@ -1,16 +1,21 @@
 package aasgard.gradle.qemu;
 
 import java.io.IOException;
+import java.nio.file.Files;
 
 import javax.inject.Inject;
 
 import org.gradle.api.Action;
+import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.TaskProvider;
 
 public abstract class QemuExtension 
 {
+	@Inject
+	public abstract ProjectLayout getLayout ();
+	
 	@Inject
 	public abstract ObjectFactory getObjects ();
 
@@ -30,6 +35,8 @@ public abstract class QemuExtension
 	public Process system (Action<? super QemuSystemSpec> configure) throws IOException
 	{
 		var spec = getObjects().newInstance(QemuSystemSpec.class);
+		spec.getTemporaryDir().set(getLayout().getBuildDirectory().dir("tmp/qemu/" + spec.hashCode()));
+		Files.createDirectories(spec.getTemporaryDir().get().getAsFile().toPath());
 		configure.execute(spec);	
 		return Qemu.system(spec);
 	}
