12ac9f6c3071c84dc6ee98ed00c98cfd24f61b23
Pedro Lamarão <pedro.lamarao@gmail.com>
2020-03-01 15:46:07 -0300

    Log GDB command

--- gradle-gdb/src/main/java/aasgard/gradle/gdb/Gdb.java
+++ gradle-gdb/src/main/java/aasgard/gradle/gdb/Gdb.java
@@ -7,17 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.gradle.api.Action;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 import org.gradle.api.provider.Provider;
 
 import aasgard.gdb.GdbMiListener;
 
 public final class Gdb 
 {
+	private static Logger logger = Logging.getLogger(Gdb.class);
+	
 	public static void exec (GdbSpec spec) throws IOException
 	{
 		final var command = new ArrayList<String>();
 		addAll(command, spec.getExecutable().get(), "--nx", "--interpreter=mi");
 		ifPresent(spec.getTarget(), it -> addAll(command, "--se", it.toString()));
+		
+		logger.info("GDB command: {}", String.join(" ", command));
 
 		final var builder = new ProcessBuilder(command);
 		spec.getEnvironment().get().forEach((key, value) -> builder.environment().put(key, value));
