56d90486d8f06031b05d1735487311b306547da1
Pedro Lamarão <pedro.lamarao@gmail.com>
2020-03-03 13:47:34 -0300

    Fully qualify std::uint32_t and std::uint64_t

--- x86/src/main/public/x86/msr.h
+++ x86/src/main/public/x86/msr.h
@@ -20,7 +20,7 @@ namespace x86
 
   //! Model specific registers
 
-  enum class msr : uint32_t
+  enum class msr : std::uint32_t
   {
     IA32_APIC_BASE   = 0x1B,
     IA32_MISC_ENABLE = 0x1A0
@@ -29,32 +29,32 @@ namespace x86
   //! Read value of model specific register
   //! @pre has_msr()
 
-  auto read_msr (uint32_t id) -> uint64_t ;
+  auto read_msr (std::uint32_t id) -> std::uint64_t ;
 
   //! Write value into model specific register
   //! @pre has_msr()
 
-  void write_msr (uint32_t id, uint32_t value);
+  void write_msr (std::uint32_t id, std::uint32_t value);
 
   //! Write value into model specific register
   //! @pre has_msr()
 
-  void write_msr (uint32_t id, uint64_t value);
+  void write_msr (std::uint32_t id, std::uint64_t value);
 
   //! Read value of model specific register
   //! @pre has_msr()
 
-  auto read_msr (msr id) -> uint64_t ;
+  auto read_msr (msr id) -> std::uint64_t ;
 
   //! Write value into model specific register
   //! @pre has_msr()
 
-  void write_msr (msr id, uint32_t value);
+  void write_msr (msr id, std::uint32_t value);
 
   //! Write value into model specific register
   //! @pre has_msr()
 
-  void write_msr (msr id, uint64_t value);
+  void write_msr (msr id, std::uint64_t value);
 
 }
 
@@ -68,15 +68,15 @@ namespace x86
 
     extern "C"
 	[[gnu::fastcall]]
-    uint64_t __read_msr ( uint32_t id );
+    std::uint64_t __read_msr ( std::uint32_t id );
 
     extern "C"
 	[[gnu::fastcall]]
-    void __write_msr_32 ( uint32_t id, uint32_t value );
+    void __write_msr_32 ( std::uint32_t id, std::uint32_t value );
 
     extern "C"
 	[[gnu::fastcall]]
-    void __write_msr_64 ( uint32_t id, uint64_t value );
+    void __write_msr_64 ( std::uint32_t id, std::uint64_t value );
 
   }
 
@@ -93,39 +93,39 @@ namespace x86
   }
 
   inline
-  auto read_msr (uint32_t id) -> uint64_t
+  auto read_msr (std::uint32_t id) -> std::uint64_t
   {
     return internal::__read_msr(id);
   }
 
   inline
-  void write_msr (uint32_t id, uint32_t value)
+  void write_msr (std::uint32_t id, std::uint32_t value)
   {
     internal::__write_msr_32(id, value);
   }
 
   inline
-  void write_msr (uint32_t id, uint64_t value)
+  void write_msr (std::uint32_t id, std::uint64_t value)
   {
     internal::__write_msr_64(id, value);
   }
 
   inline
-  auto read_msr (msr id) -> uint64_t
+  auto read_msr (msr id) -> std::uint64_t
   {
-    return read_msr((uint32_t)(id));
+    return read_msr((std::uint32_t)(id));
   }
 
   inline
-  void write_msr (msr id, uint32_t value)
+  void write_msr (msr id, std::uint32_t value)
   {
-    write_msr((uint32_t)(id), value);
+    write_msr((std::uint32_t)(id), value);
   }
 
   inline
-  void write_msr (msr id, uint64_t value)
+  void write_msr (msr id, std::uint64_t value)
   {
-    write_msr((uint32_t)(id), value);
+    write_msr((std::uint32_t)(id), value);
   }
 
 }
