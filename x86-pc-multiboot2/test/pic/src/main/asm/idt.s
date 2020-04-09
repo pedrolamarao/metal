4882184bebdbe42cdbdfb21908db5189fb144624
Pedro Lamarão <pedro.lamarao@gmail.com>
2020-04-08 22:35:14 -0300

    Remove unnecessary and/or obsolete directives

--- x86-pc-multiboot2/test/pic/src/main/asm/idt.s
+++ x86-pc-multiboot2/test/pic/src/main/asm/idt.s
@@ -1,7 +1,5 @@
 .att_syntax
 
-.text
-
 //! extern "C" std::uint32_t volatile _test_result
 
 .extern _test_result
@@ -10,8 +8,6 @@
 
 .global __interrupt_service_routine
 .type   __interrupt_service_routine, STT_FUNC
-.func   __interrupt_service_routine
 __interrupt_service_routine:
     movl $0, _test_result
 	iret
-.endfunc
