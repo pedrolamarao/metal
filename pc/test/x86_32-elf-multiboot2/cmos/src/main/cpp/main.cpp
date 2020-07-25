0bd686f1b7d033f46e0db9ddfe45348e9c44b5a2
Pedro Lamarão <pedro.lamarao@gmail.com>
2020-07-24 23:04:08 -0300

    Properly configure system emulator for CMOS RTC tests

--- pc/test/x86_32-elf-multiboot2/cmos/src/main/cpp/main.cpp
+++ pc/test/x86_32-elf-multiboot2/cmos/src/main/cpp/main.cpp
@@ -43,39 +43,61 @@ extern "C"
 void main ( ps::size4 magic, multiboot2::information_list & mbi )
 {
     pc::cmos<x86::port> cmos;
+    
+    // Note: CMOS RTC values must be fixed by the system emulator
+    
+    auto status_B = cmos.read(0x0B, false);
+    
+    // CMOS RTC time format
+    
+    auto format_24 = (status_B & 0x02) == 0x02;
+    
+    if (! format_24) {
+    	_test_result = 2;
+    	return;
+    }
+    
+    // CMOS RTC date format
+    
+    auto format_Binary = (status_B & 0x04) == 0x04;
+
+    if (format_Binary) {
+    	_test_result = 3;
+    	return;
+    }
 
     // CMOS RTC year
 
-    if (cmos.read(0x09, false) != 01) {
-    	_test_result = 2;
+    if (cmos.read(0x09, false) != 0x20) {
+    	_test_result = 10;
     	return;
     }
 
     // CMOS RTC month
 
-    if (cmos.read(0x08, false) != 02) {
-    	_test_result = 3;
+    if (cmos.read(0x08, false) != 0x07) {
+    	_test_result = 11;
     	return;
     }
 
     // CMOS RTC day of month
 
-    if (cmos.read(0x07, false) != 03) {
-    	_test_result = 4;
+    if (cmos.read(0x07, false) != 0x24) {
+    	_test_result = 12;
     	return;
     }
 
     // CMOS RTC hour
 
-    if (cmos.read(0x04, false) != 04) {
-    	_test_result = 5;
+    if (cmos.read(0x04, false) != 0x22) {
+    	_test_result = 20;
     	return;
     }
 
     // CMOS RTC minute
 
-    if (cmos.read(0x02, false) != 05) {
-    	_test_result = 6;
+    if (cmos.read(0x02, false) != 0x46) {
+    	_test_result = 21;
     	return;
     }
 
