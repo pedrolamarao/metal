16f1b235fa6cc11bef0f27d7676d8af001fbfe6a
Pedro Lamarão <pedro.lamarao@prodist.com.br>
2021-08-12 15:02:50 -0300

    Update test.

--- pc/test/x86_32-elf-multiboot2/pit/src/main/asm/idt.s
+++ pc/test/x86_32-elf-multiboot2/pit/src/main/asm/idt.s
@@ -1,43 +1,90 @@
-# Copyright (C) 2021 Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
+# Copyright (C) 2020,2021 Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
 
 .att_syntax
 
-.global _x86_exception
-.type   _x86_exception, STT_FUNC
-_x86_exception:
-    incl _x86_exception_counter
-	iret
+.macro __x86_trap NN
+ .global __x86_exception_\NN\()
+ .type   __x86_exception_\NN\(), @function
+ .align 4
+ __x86_exception_\NN\():
+     incl __x86_exception_counter
+     iret
+.endm
 
-.global _x86_interrupt_master
-.type   _x86_interrupt_master, STT_FUNC
-_x86_interrupt_master:
-    incl _x86_interrupt_master_counter
+.macro __x86_fault NN
+ .global __x86_exception_\NN\()
+ .type   __x86_exception_\NN\(), @function
+ .align 4
+ __x86_exception_\NN\():
+     incl __x86_exception_counter
+     iret
+.endm
+
+.macro __x86_fault_code NN
+ .global __x86_exception_\NN\()
+ .type   __x86_exception_\NN\(), @function
+ .align 4
+ __x86_exception_\NN\():
+     incl __x86_exception_counter
+     add $4, %esp
+     iret
+.endm
+
+__x86_fault      00
+__x86_fault      01
+__x86_trap       02
+__x86_trap       03
+__x86_trap       04
+__x86_fault      05
+__x86_fault      06
+__x86_fault      07
+__x86_fault_code 08
+__x86_fault      09
+__x86_fault_code 0A
+__x86_fault_code 0B
+__x86_fault_code 0C
+__x86_fault_code 0D
+__x86_fault_code 0E
+__x86_trap       0F
+__x86_fault      10
+__x86_fault_code 11
+__x86_fault      12
+__x86_fault      13
+__x86_fault      14
+__x86_fault_code 15
+__x86_trap       16
+__x86_trap       17
+__x86_trap       18
+__x86_trap       19
+__x86_trap       1A
+__x86_trap       1B
+__x86_trap       1C
+__x86_trap       1D
+__x86_trap       1E
+__x86_trap       1F
+
+.global __x86_interrupt_master
+.type   __x86_interrupt_master, STT_FUNC
+.align 4
+__x86_interrupt_master:
+    incl __x86_interrupt_master_counter
     mov $0x20, %al
-    out %al, $0x20
+    outb %al, $0x20
 	iret
 
-.global _x86_interrupt_slave
-.type   _x86_interrupt_slave, STT_FUNC
-_x86_interrupt_slave:
-    incl _x86_interrupt_slave_counter
+.global __x86_interrupt_slave
+.type   __x86_interrupt_slave, STT_FUNC
+.align 4
+__x86_interrupt_slave:
+    incl __x86_interrupt_slave_counter
     mov $0x20, %al
-    out %al, $0xA0
-    out %al, $0x20
+    outb %al, $0xA0
+    outb %al, $0x20
 	iret
 
-.global _x86_interrupt_free
-.type   _x86_interrupt_free, STT_FUNC
-_x86_interrupt_free:
-    incl _x86_interrupt_free_counter
+.global __x86_interrupt
+.type   __x86_interrupt, STT_FUNC
+.align 4
+__x86_interrupt:
+    incl __x86_interrupt_counter
 	iret
-
-.global _test_sandbox
-.type   _test_sandbox, STT_FUNC
-_test_sandbox:
-    mov $0x34, %al
-    out %al, $0x43
-    mov $0x10000, %ax
-    out %al, $0x40
-    mov %ah, %al
-    out %al, $0x40
-    ret
--- pc/test/x86_32-elf-multiboot2/pit/src/main/cpp/main.cpp
+++ pc/test/x86_32-elf-multiboot2/pit/src/main/cpp/main.cpp
@@ -16,58 +16,36 @@
 #include <pc/pit.h>
 
 
-//! Multiboot2 minimal request for ELF program
+//! Multiboot2
 
-namespace
+namespace multiboot2
 {
     struct request_type
     {
-        multiboot2::header_prologue prologue;
-        multiboot2::end_request     end;
+        header_prologue prologue;
+        end_request     end;
     };
 
     [[gnu::used, gnu::section(".multiboot2")]]
     constexpr request_type request =
     {
-        { multiboot2::architecture_type::x86, sizeof(request), },
+        { architecture_type::x86, sizeof(request), },
         { },
     };
 }
 
-//! x86 protected-mode GDT
+//! x86
 
-namespace
+namespace x86
 {
-    [[gnu::section(".gdt")]]
-    constexpr x86::segment_descriptor global_descriptor_table [5] =
-    {
-        { },
-        { 0, 0xFFFFFFFF, x86::code_segment_access(true, false, 0), x86::segment_granularity(false, true, true) },
-        { 0, 0xFFFFFFFF, x86::data_segment_access(true, false, 0), x86::segment_granularity(false, true, true) },
-        { 0, 0xFFFFFFFF, x86::code_segment_access(true, false, 3), x86::segment_granularity(false, true, true) },
-        { 0, 0xFFFFFFFF, x86::data_segment_access(true, false, 3), x86::segment_granularity(false, true, true) },
-    };
-}
+    void set_global_descriptor_table_register ();
 
-//! x86 protected-mode IDT
+    extern "C" [[gnu::used]] unsigned __x86_exception_counter {};
+    extern "C" [[gnu::used]] unsigned __x86_interrupt_master_counter {};
+    extern "C" [[gnu::used]] unsigned __x86_interrupt_slave_counter {};
+    extern "C" [[gnu::used]] unsigned __x86_interrupt_counter {};
 
-namespace
-{
-    [[gnu::section(".idt")]]
-    x86::interrupt_gate_descriptor interrupt_descriptor_table [256] =
-    { };
-
-    extern "C" void _x86_exception ();
-    extern "C" [[gnu::used]] unsigned volatile _x86_exception_counter {};
-
-    extern "C" void _x86_interrupt_master ();
-    extern "C" [[gnu::used]] unsigned volatile _x86_interrupt_master_counter {};
-
-    extern "C" void _x86_interrupt_slave ();
-    extern "C" [[gnu::used]] unsigned volatile _x86_interrupt_slave_counter {};
-
-    extern "C" void _x86_interrupt_free ();
-    extern "C" [[gnu::used]] unsigned volatile _x86_interrupt_free_counter {};
+    void set_interrupt_descriptor_table_register ();
 }
 
 //! Psys test protocol
@@ -75,6 +53,7 @@ namespace
 namespace
 {
     extern "C" [[gnu::used]] unsigned volatile _test_control {};
+    extern "C" [[gnu::used]] unsigned volatile _test_debug {};
 }
 
 //! Multiboot2 entry point
@@ -82,30 +61,46 @@ namespace
 extern "C"
 void main ( ps::size4 magic, multiboot2::information_list & mbi )
 {
+    using namespace multiboot2;
+    using namespace x86;
+
+    // test: verify boot sanity
+
     _test_control = 1;
 
-    // gdt
-
-    x86::set_global_descriptor_table(global_descriptor_table);
-    x86::reload_segment_registers(x86::segment_selector(1, false, 0), x86::segment_selector(2, false, 0));
-
-    // idt
-
-    for (auto i = 0U, j = 32U; i != j; ++i) {
-        interrupt_descriptor_table[i] = { 0x8, _x86_exception, x86::interrupt_gate_access(true, 0) };
+    if (magic != multiboot2::information_magic) {
+        _test_control = 0;
+        return;
     }
-    for (auto i = 32U, j = 40U; i != j; ++i) {
-        interrupt_descriptor_table[i] = { 0x8, _x86_interrupt_master, x86::interrupt_gate_access(true, 0) };
-    }
-    for (auto i = 40U, j = 48U; i != j; ++i) {
-        interrupt_descriptor_table[i] = { 0x8, _x86_interrupt_slave, x86::interrupt_gate_access(true, 0) };
-    }
-    for (auto i = 48U, j = 256U; i != j; ++i) {
-        interrupt_descriptor_table[i] = { 0x8, _x86_interrupt_free, x86::interrupt_gate_access(true, 0) };
-    }
-    x86::set_interrupt_descriptor_table(interrupt_descriptor_table, 256);
 
-    // pic
+    // set the GDT register and set segment registers
+
+    _test_control = 2;
+
+    set_global_descriptor_table_register();
+
+    // set the IDT register
+
+    _test_control = 3;
+
+    set_interrupt_descriptor_table_register();
+
+    enable_interrupts();
+
+    // test: verify interrupt sanity
+
+    _test_control = 4;
+
+    x86::interrupt<0x30>();
+
+    if (__x86_interrupt_counter == 0) {
+        _test_control = 0;
+        return;
+    }
+
+    // initialize PIC
+
+    _test_control = 5;
 
     auto master = pc::master_pic<x86::port>::create();
     auto slave  = pc::slave_pic<x86::port>::create();
@@ -118,6 +113,11 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
     slave.icw3(2);
     master.icw4(true, false, pc::pic_buffering::none, false);
     slave.icw4(true, false, pc::pic_buffering::none, false);
+
+    // unmask PIC IRQ 0
+
+    _test_control = 6;
+
     master.ocw1(0xFE);
     slave.ocw1(0xFF);
 
@@ -125,9 +125,9 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
 
     auto pit = pc::pit<x86::port>::create();
 
-    // test: configure command builder
+    // test: 'configure' command builder
 
-    _test_control = 2;
+    _test_control = 10;
 
     auto configure = pc::pit_configure_command(pc::pit_counter::zero, pc::pit_access::low, pc::pit_mode::hardware_retriggerable_one_shot, pc::pit_format::binary);
     if (configure != 0b00010010) {
@@ -136,9 +136,9 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
         return;
     }
 
-    // test: read_back command builder
+    // test: 'read back' command builder
 
-    _test_control = 3;
+    _test_control = 11;
 
     auto read_back = pc::pit_read_back_command(false, true, pc::pit_counter::zero);
     if (read_back != 0b11010010) {
@@ -150,7 +150,7 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
     // test: PIT mode 0: configure, read_back
     // assumption: test is faster than counter
 
-    _test_control = 100;
+    _test_control = 20;
 
     pit.configure(pc::pit_counter::zero, pc::pit_access::low, pc::pit_mode::interrupt_on_terminal_count, pc::pit_format::binary);
     pit.counter_0(0);
@@ -165,7 +165,7 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
     // test: PIT mode 1: configure, read_back
     // assumption: test is faster than counter
 
-    _test_control = 101;
+    _test_control = 21;
 
     pit.configure(pc::pit_counter::zero, pc::pit_access::low, pc::pit_mode::hardware_retriggerable_one_shot, pc::pit_format::binary);
     pit.counter_0(0);
@@ -181,7 +181,7 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
     // test: PIT mode 2: configure, read_back
     // assumption: test is faster than counter
 
-    _test_control = 102;
+    _test_control = 22;
 
     pit.configure(pc::pit_counter::zero, pc::pit_access::low, pc::pit_mode::rate_generator, pc::pit_format::binary);
     pit.counter_0(0);
@@ -197,7 +197,7 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
     // test: PIT mode 3: configure, read_back
     // assumption: test is faster than counter
 
-    _test_control = 103;
+    _test_control = 23;
 
     pit.configure(pc::pit_counter::zero, pc::pit_access::low, pc::pit_mode::square_wave_generator, pc::pit_format::binary);
     pit.counter_0(0);
@@ -213,7 +213,7 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
     // test: PIT mode 4: configure, read_back
     // assumption: test is faster than counter
 
-    _test_control = 104;
+    _test_control = 24;
 
     pit.configure(pc::pit_counter::zero, pc::pit_access::low, pc::pit_mode::software_triggered_strobe, pc::pit_format::binary);
     pit.counter_0(0);
@@ -229,7 +229,7 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
     // test: PIT mode 5: configure, read_back
     // assumption: test is faster than counter
 
-    _test_control = 105;
+    _test_control = 25;
 
     pit.configure(pc::pit_counter::zero, pc::pit_access::low, pc::pit_mode::hardware_triggered_strobe, pc::pit_format::binary);
     pit.counter_0(0);
@@ -244,4 +244,129 @@ void main ( ps::size4 magic, multiboot2::information_list & mbi )
 
     _test_control = -1;
     return;
+}
+
+//! Definitions
+
+namespace x86
+{
+    // GDT
+
+    [[gnu::section(".gdt")]]
+    constexpr segment_descriptor global_descriptor_table [5] =
+    {
+        { },
+        { 0, 0xFFFFFFFF, code_segment_access(true, false, 0), segment_granularity(false, true, true) },
+        { 0, 0xFFFFFFFF, data_segment_access(true, false, 0), segment_granularity(false, true, true) },
+        { 0, 0xFFFFFFFF, code_segment_access(true, false, 3), segment_granularity(false, true, true) },
+        { 0, 0xFFFFFFFF, data_segment_access(true, false, 3), segment_granularity(false, true, true) },
+    };
+
+    void set_global_descriptor_table_register ()
+    {
+        set_global_descriptor_table_register(global_descriptor_table);
+        auto const cs = segment_selector(1, false, 0);
+        set_code_segment_register(cs);
+        auto const ds = segment_selector(2, false, 0).value();
+        asm volatile ("mov %0, %%ds" : : "r"(ds));
+        asm volatile ("mov %0, %%ss" : : "r"(ds));
+        asm volatile ("mov %0, %%es" : : "r"(ds));
+        asm volatile ("mov %0, %%fs" : : "r"(ds));
+        asm volatile ("mov %0, %%gs" : : "r"(ds));
+    }
+
+    // IDT
+
+    extern "C" void __x86_exception_00 ();
+    extern "C" void __x86_exception_01 ();
+    extern "C" void __x86_exception_02 ();
+    extern "C" void __x86_exception_03 ();
+    extern "C" void __x86_exception_04 ();
+    extern "C" void __x86_exception_05 ();
+    extern "C" void __x86_exception_06 ();
+    extern "C" void __x86_exception_07 ();
+    extern "C" void __x86_exception_08 ();
+    extern "C" void __x86_exception_09 ();
+    extern "C" void __x86_exception_0A ();
+    extern "C" void __x86_exception_0B ();
+    extern "C" void __x86_exception_0C ();
+    extern "C" void __x86_exception_0D ();
+    extern "C" void __x86_exception_0E ();
+    extern "C" void __x86_exception_0F ();
+    extern "C" void __x86_exception_10 ();
+    extern "C" void __x86_exception_10 ();
+    extern "C" void __x86_exception_11 ();
+    extern "C" void __x86_exception_12 ();
+    extern "C" void __x86_exception_13 ();
+    extern "C" void __x86_exception_14 ();
+    extern "C" void __x86_exception_15 ();
+    extern "C" void __x86_exception_16 ();
+    extern "C" void __x86_exception_17 ();
+    extern "C" void __x86_exception_18 ();
+    extern "C" void __x86_exception_19 ();
+    extern "C" void __x86_exception_1A ();
+    extern "C" void __x86_exception_1B ();
+    extern "C" void __x86_exception_1C ();
+    extern "C" void __x86_exception_1D ();
+    extern "C" void __x86_exception_1E ();
+    extern "C" void __x86_exception_1F ();
+    extern "C" void __x86_interrupt_master ();
+    extern "C" void __x86_interrupt_slave ();
+    extern "C" void __x86_interrupt ();
+
+    [[gnu::section(".idt")]]
+    interrupt_gate_descriptor interrupt_descriptor_table [256];
+
+    void set_interrupt_descriptor_table_register ()
+    {
+        auto const interrupt_segment = segment_selector(1, false, 0);
+        auto const interrupt_access = interrupt_gate_access(true, 0);
+
+        interrupt_descriptor_table[0x00] = { interrupt_segment, __x86_exception_00, interrupt_access };
+        interrupt_descriptor_table[0x01] = { interrupt_segment, __x86_exception_01, interrupt_access };
+        interrupt_descriptor_table[0x02] = { interrupt_segment, __x86_exception_02, interrupt_access };
+        interrupt_descriptor_table[0x03] = { interrupt_segment, __x86_exception_03, interrupt_access };
+        interrupt_descriptor_table[0x04] = { interrupt_segment, __x86_exception_04, interrupt_access };
+        interrupt_descriptor_table[0x05] = { interrupt_segment, __x86_exception_05, interrupt_access };
+        interrupt_descriptor_table[0x06] = { interrupt_segment, __x86_exception_06, interrupt_access };
+        interrupt_descriptor_table[0x07] = { interrupt_segment, __x86_exception_07, interrupt_access };
+        interrupt_descriptor_table[0x08] = { interrupt_segment, __x86_exception_08, interrupt_access };
+        interrupt_descriptor_table[0x09] = { interrupt_segment, __x86_exception_09, interrupt_access };
+        interrupt_descriptor_table[0x0A] = { interrupt_segment, __x86_exception_0A, interrupt_access };
+        interrupt_descriptor_table[0x0B] = { interrupt_segment, __x86_exception_0B, interrupt_access };
+        interrupt_descriptor_table[0x0C] = { interrupt_segment, __x86_exception_0C, interrupt_access };
+        interrupt_descriptor_table[0x0D] = { interrupt_segment, __x86_exception_0D, interrupt_access };
+        interrupt_descriptor_table[0x0E] = { interrupt_segment, __x86_exception_0E, interrupt_access };
+        interrupt_descriptor_table[0x0F] = { interrupt_segment, __x86_exception_0F, interrupt_access };
+        interrupt_descriptor_table[0x10] = { interrupt_segment, __x86_exception_10, interrupt_access };
+        interrupt_descriptor_table[0x11] = { interrupt_segment, __x86_exception_11, interrupt_access };
+        interrupt_descriptor_table[0x12] = { interrupt_segment, __x86_exception_12, interrupt_access };
+        interrupt_descriptor_table[0x13] = { interrupt_segment, __x86_exception_13, interrupt_access };
+        interrupt_descriptor_table[0x14] = { interrupt_segment, __x86_exception_14, interrupt_access };
+        interrupt_descriptor_table[0x15] = { interrupt_segment, __x86_exception_15, interrupt_access };
+        interrupt_descriptor_table[0x16] = { interrupt_segment, __x86_exception_16, interrupt_access };
+        interrupt_descriptor_table[0x17] = { interrupt_segment, __x86_exception_17, interrupt_access };
+        interrupt_descriptor_table[0x18] = { interrupt_segment, __x86_exception_18, interrupt_access };
+        interrupt_descriptor_table[0x19] = { interrupt_segment, __x86_exception_19, interrupt_access };
+        interrupt_descriptor_table[0x1A] = { interrupt_segment, __x86_exception_1A, interrupt_access };
+        interrupt_descriptor_table[0x1B] = { interrupt_segment, __x86_exception_1B, interrupt_access };
+        interrupt_descriptor_table[0x1C] = { interrupt_segment, __x86_exception_1C, interrupt_access };
+        interrupt_descriptor_table[0x1D] = { interrupt_segment, __x86_exception_1D, interrupt_access };
+        interrupt_descriptor_table[0x1E] = { interrupt_segment, __x86_exception_1E, interrupt_access };
+        interrupt_descriptor_table[0x1F] = { interrupt_segment, __x86_exception_1F, interrupt_access };
+
+        for (auto i = 32U, j = 40U; i != j; ++i) {
+           interrupt_descriptor_table[i] = { interrupt_segment, __x86_interrupt_master, interrupt_access };
+        }
+
+        for (auto i = 40U, j = 48U; i != j; ++i) {
+           interrupt_descriptor_table[i] = { interrupt_segment, __x86_interrupt_slave, interrupt_access };
+        }
+
+        for (auto i = 48U, j = 256U; i != j; ++i) {
+           interrupt_descriptor_table[i] = { interrupt_segment, __x86_interrupt, interrupt_access };
+        }
+
+        set_interrupt_descriptor_table_register(interrupt_descriptor_table);
+    }
 }
