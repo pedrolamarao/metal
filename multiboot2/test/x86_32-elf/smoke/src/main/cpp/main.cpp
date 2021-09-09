1db4ca0f5c74c3e658bf6def3941441fb5c6d139
Pedro Lamarão <pedro.lamarao@prodist.com.br>
2021-09-09 17:50:36 -0300

    Add system tests by enumerating the system tests folder.

--- multiboot2/test/x86_32-elf/smoke/src/main/cpp/main.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2020 Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
-
-//! Psys test protocol
-
-extern "C"
-{
-	[[gnu::used]] unsigned volatile _test_control {};
-	[[gnu::used]] unsigned volatile _test_debug {};
-}
