428e50f24a71b2cbd1bf4b0f28394d5d77a218a8
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-03-11 20:57:34 -0300

    Move project build scripts to Kotlin.

---
+++ app/sandbox/build.gradle.kts
@@ -0,0 +1,45 @@
+import dev.nokee.platform.nativebase.ExecutableBinary
+
+plugins {
+    id("psys-application")
+}
+
+application {
+    // #XXX: Nokee can't cross compile to system "none"
+    val none = org.gradle.internal.os.OperatingSystem.current().getName()
+    targetMachines.add( machines.os(none).architecture("-multiboot-x86_32") )
+
+    dependencies {
+        implementation(project(":multiboot2"))
+        implementation(project(":x86"))
+    }
+
+    val baseArgs = listOf("-std=c++20", "-flto", "-fasm-blocks")
+
+    binaries.configureEach {
+        if (this is ExecutableBinary) {
+            compileTasks.configureEach {
+                compilerArgs.addAll(baseArgs)
+            }
+            linkTask {
+                linkerArgs.addAll(baseArgs)
+            }
+        }
+    }
+}
+
+val createImage = project.tasks.register<MultibootCreateImageTask>("createImage") {
+    group = "psys"
+    dependsOn(project.tasks.assemble)
+    inputFile.set(
+        project.provider {
+            val executable = application.binaries.get().stream().findAny().get() as ExecutableBinary
+            executable.linkTask.get().linkedFile.get()
+        }
+    )
+}
+
+project.tasks.register<MultibootRunImageTask>("runImage") {
+    group = "psys"
+    imageFile.set( createImage.get().outputFile )
+}
