14d0fe0dc55deec246bcbdb20e3d900c8a5ef7b0
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-03-12 13:03:01 -0300

    Move last build scripts to Kotlin.

---
+++ x86/test/x86_32-elf-multiboot2/build.gradle.kts
@@ -0,0 +1,36 @@
+import dev.nokee.platform.nativebase.ExecutableBinary
+
+plugins {
+    base
+}
+
+subprojects {
+    apply(plugin = "psys-test")
+
+    project.extensions.configure<dev.nokee.platform.cpp.CppApplication> {
+        // #XXX: Nokee can't cross compile to system "none"
+        val none = org.gradle.internal.os.OperatingSystem.current().getName()
+        targetMachines.addAll(
+            machines.os(none).architecture("-multiboot-x86_32")
+        )
+
+        dependencies {
+            implementation(project(":multiboot2"))
+            implementation(project(":x86"))
+            implementation(project(":x86:test"))
+        }
+
+        val baseArgs = listOf("-std=c++20", "-flto", "-fasm-blocks")
+
+        binaries.configureEach {
+            if (this is ExecutableBinary) {
+                compileTasks.configureEach {
+                    compilerArgs.addAll(baseArgs)
+                }
+                linkTask {
+                    linkerArgs.addAll(baseArgs)
+                }
+            }
+        }
+    }
+}
