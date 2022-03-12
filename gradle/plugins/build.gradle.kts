f98a5f371b7b7c6b9d5f93c1032aaec92938b70e
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-03-12 13:02:14 -0300

    Explicitly set Java and Kotlin JVM target.

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -1,8 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
 }
 
 val nokee = "0.4.0-202112012027.ff552c41"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 dependencies {
     implementation("br.dev.pedrolamarao.gdb:core:1.0.0-SNAPSHOT")
@@ -11,4 +15,10 @@ dependencies {
     implementation("dev.nokee.cpp-application:dev.nokee.cpp-application.gradle.plugin:${nokee}")
     implementation("dev.nokee.cpp-library:dev.nokee.cpp-library.gradle.plugin:${nokee}")
     implementation("dev.nokee.native-unit-testing:dev.nokee.native-unit-testing.gradle.plugin:${nokee}")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
