f778104de79d2b8e1d7a46a77e081a197612d8f8
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-03-23 18:38:11 -0300

    Add PAE page directory entry for large pages.

--- x86/src/main/public/x86/_32/page.h
+++ x86/src/main/public/x86/_32/page.h
@@ -87,7 +87,7 @@ namespace x86::_32
 
         auto available () const -> unsigned _ExtInt(3);
 
-        //! Page base address.
+        //! Page address.
 
         auto address () const -> size4;
     };
@@ -238,7 +238,7 @@ namespace x86::_32
 
         auto attribute () const -> unsigned _ExtInt(1);
 
-        //! Page base address.
+        //! Page address.
 
         auto address () const -> size8;
     };
@@ -323,7 +323,7 @@ namespace x86::_32
 
         auto available () const -> unsigned _ExtInt(3);
 
-        //! Page base address.
+        //! Page address.
 
         auto address () const -> size8;
 
@@ -367,27 +367,27 @@ namespace x86::_32
             unsigned _ExtInt(1)  executable
         );
 
-        //! Page table is present in memory.
+        //! Page table entry is present in memory.
 
         auto present () const -> bool;
 
-        //! Page table is writable.
+        //! Page table entry is writable.
 
         auto writable () const -> bool;
 
-        //! Page table is accessible by user (i.e. DPL 0).
+        //! Page table entry is accessible by user (i.e. DPL 0).
 
         auto user () const -> bool;
 
-        //! Page table has write through.
+        //! Page table entry has write through.
 
         auto write_through () const -> bool;
 
-        //! Page table has cache.
+        //! Page table entry has cache.
 
         auto cache () const -> bool;
 
-        //! Page table was accessed.
+        //! Page table entry was accessed.
 
         auto accessed () const -> bool;
 
@@ -395,11 +395,11 @@ namespace x86::_32
 
         auto available () const -> unsigned _ExtInt(3);
 
-        //! Page base address.
+        //! Page table address.
 
         auto address () const -> size8;
 
-        //! Page is executable.
+        //! Page table entry is executable.
 
         auto executable () const -> bool;
     };
@@ -432,15 +432,15 @@ namespace x86::_32
             unsigned _ExtInt(40) address
         );
 
-        //! Page table is present in memory.
+        //! Page directory is present in memory.
 
         auto present () const -> bool;
 
-        //! Page table has write through.
+        //! Page directory has write through.
 
         auto write_through () const -> bool;
 
-        //! Page table has cache.
+        //! Page directory has cache.
 
         auto cache () const -> bool;
 
@@ -448,12 +448,103 @@ namespace x86::_32
 
         auto available () const -> unsigned _ExtInt(3);
 
-        //! Page base address.
+        //! Page directory address.
 
         auto address () const -> size8;
     };
 
     static_assert(sizeof(long_small_page_pointer_entry) == 8, "unexpected size of long_small_page_pointer_entry");
+
+    //! 64 bit, 2 MiB page directory entry.
+
+    class long_large_page_directory_entry
+    {
+        size8 _present       :  1;
+        size8 _writable      :  1;
+        size8 _user          :  1;
+        size8 _write_through :  1;
+        size8 _cache         :  1;
+        size8 _accessed      :  1;
+        size8 _dirty         :  1;
+        size8 _long          :  1 = 1;
+        size8 _global        :  1;
+        size8 _available     :  3;
+        size8 _attribute     :  1;
+        size8 _zero_0        :  8 = 0;
+        size8 _address       : 31;
+        size8 _zero_1        : 11 = 0;
+        size8 _executable    :  1;
+
+    public:
+
+        //! Field constructor.
+
+        constexpr
+        long_large_page_directory_entry (
+            unsigned _ExtInt(1)  present,
+            unsigned _ExtInt(1)  writable,
+            unsigned _ExtInt(1)  user,
+            unsigned _ExtInt(1)  write_through,
+            unsigned _ExtInt(1)  cache,
+            unsigned _ExtInt(1)  accessed,
+            unsigned _ExtInt(1)  dirty,
+            unsigned _ExtInt(1)  global,
+            unsigned _ExtInt(3)  available,
+            unsigned _ExtInt(1)  attribute,
+            unsigned _ExtInt(31) address,
+            unsigned _ExtInt(1)  executable
+        );
+
+        //! Page is present in memory.
+
+        auto present () const -> bool;
+
+        //! Page is writable.
+
+        auto writable () const -> bool;
+
+        //! Page is accessible by user (i.e. DPL 0).
+
+        auto user () const -> bool;
+
+        //! Page has write through.
+
+        auto write_through () const -> bool;
+
+        //! Page has cache.
+
+        auto cache () const -> bool;
+
+        //! Page was accessed.
+
+        auto accessed () const -> bool;
+
+        //! Page is dirty.
+
+        auto dirty () const -> bool;
+
+        //! Page is global.
+
+        auto global () const -> bool;
+
+        //! Bits available to software.
+
+        auto available () const -> unsigned _ExtInt(3);
+
+        //! Page attribute high bit.
+
+        auto attribute () const -> unsigned _ExtInt(1);
+
+        //! Page address.
+
+        auto address () const -> size8;
+
+        //! Page is executable.
+
+        auto executable () const -> bool;
+    };
+
+    static_assert(sizeof(long_large_page_directory_entry) == 8, "unexpected size of long_large_page_directory_entry");
 }
 
 // Implementation.
@@ -518,7 +609,7 @@ namespace x86::_32
     auto short_small_page_table_entry::available () const -> unsigned _ExtInt(3) { return _available; }
 
     inline
-    auto short_small_page_table_entry::address () const -> size4 { return _address << 12; }
+    auto short_small_page_table_entry::address () const -> size4 { return size4{_address} << 12; }
 
     inline constexpr
     short_small_page_directory_entry::short_small_page_directory_entry (
@@ -563,7 +654,7 @@ namespace x86::_32
     auto short_small_page_directory_entry::available () const -> unsigned _ExtInt(3) { return _available; }
 
     inline
-    auto short_small_page_directory_entry::address () const -> size4 { return _address << 12; }
+    auto short_small_page_directory_entry::address () const -> size4 { return size4{_address} << 12; }
 
     inline constexpr
     short_large_page_directory_entry::short_large_page_directory_entry (
@@ -687,7 +778,7 @@ namespace x86::_32
     auto long_small_page_table_entry::available () const -> unsigned _ExtInt(3) { return _available; }
 
     inline
-    auto long_small_page_table_entry::address () const -> size8 { return _address << 12; }
+    auto long_small_page_table_entry::address () const -> size8 { return size8{_address} << 12; }
 
     inline
     auto long_small_page_table_entry::executable () const -> bool { return _executable; }
@@ -737,7 +828,7 @@ namespace x86::_32
     auto long_small_page_directory_entry::available () const -> unsigned _ExtInt(3) { return _available; }
 
     inline
-    auto long_small_page_directory_entry::address () const -> size8 { return _address << 12; }
+    auto long_small_page_directory_entry::address () const -> size8 { return size8{_address} << 12; }
 
     inline
     auto long_small_page_directory_entry::executable () const -> bool { return _executable; }
@@ -770,5 +861,70 @@ namespace x86::_32
     auto long_small_page_pointer_entry::available () const -> unsigned _ExtInt(3) { return _available; }
 
     inline
-    auto long_small_page_pointer_entry::address () const -> size8 { return _address << 12; }
+    auto long_small_page_pointer_entry::address () const -> size8 { return size8{_address} << 12; }
+
+    inline constexpr
+    long_large_page_directory_entry::long_large_page_directory_entry (
+        unsigned _ExtInt(1)  present,
+        unsigned _ExtInt(1)  writable,
+        unsigned _ExtInt(1)  user,
+        unsigned _ExtInt(1)  write_through,
+        unsigned _ExtInt(1)  cache,
+        unsigned _ExtInt(1)  accessed,
+        unsigned _ExtInt(1)  dirty,
+        unsigned _ExtInt(1)  global,
+        unsigned _ExtInt(3)  available,
+        unsigned _ExtInt(1)  attribute,
+        unsigned _ExtInt(31) address,
+        unsigned _ExtInt(1)  executable
+    ) :
+        _present{present},
+        _writable{writable},
+        _user{user},
+        _write_through{write_through},
+        _cache{cache},
+        _accessed{accessed},
+        _dirty{dirty},
+        _global{global},
+        _available{available},
+        _attribute{attribute},
+        _address{address},
+        _executable{executable}
+    { }
+
+    inline
+    auto long_large_page_directory_entry::present () const -> bool { return _present; }
+
+    inline
+    auto long_large_page_directory_entry::writable () const -> bool { return _writable; }
+
+    inline
+    auto long_large_page_directory_entry::user () const -> bool { return _user; }
+
+    inline
+    auto long_large_page_directory_entry::write_through () const -> bool { return _write_through; }
+
+    inline
+    auto long_large_page_directory_entry::cache () const -> bool { return _cache; }
+
+    inline
+    auto long_large_page_directory_entry::accessed () const -> bool { return _accessed; }
+
+    inline
+    auto long_large_page_directory_entry::dirty () const -> bool { return _dirty; }
+
+    inline
+    auto long_large_page_directory_entry::global () const -> bool { return _global; }
+
+    inline
+    auto long_large_page_directory_entry::available () const -> unsigned _ExtInt(3) { return _available; }
+
+    inline
+    auto long_large_page_directory_entry::attribute () const -> unsigned _ExtInt(1) { return _attribute; }
+
+    inline
+    auto long_large_page_directory_entry::address () const -> size8 { return size8{_address} << 21; }
+
+    inline
+    auto long_large_page_directory_entry::executable () const -> bool { return _executable; }
 }
--- x86/src/test/cpp/page.cpp
+++ x86/src/test/cpp/page.cpp
@@ -1336,7 +1336,6 @@ namespace x86::_32
     TEST(long_small_page_table_entry, executable)
     {
         size8 memory = size8{1} << 63;
-
         auto& reference = reinterpret_cast<long_small_page_table_entry&>(memory);
         ASSERT_FALSE(reference.present());
         ASSERT_FALSE(reference.writable());
@@ -1605,7 +1604,6 @@ namespace x86::_32
     TEST(long_small_page_directory_entry, executable)
     {
         size8 memory = size8{1} << 63;
-
         auto& reference = reinterpret_cast<long_small_page_directory_entry&>(memory);
         ASSERT_FALSE(reference.present());
         ASSERT_FALSE(reference.writable());
@@ -1737,5 +1735,423 @@ namespace x86::_32
         ASSERT_FALSE(fields.cache());
         ASSERT_EQ(0,fields.available());
         ASSERT_EQ(0xFEDCBA9876000,fields.address());
+    }
+
+    // long_large_page_directory_entry
+
+    TEST(long_large_page_directory_entry, zero)
+    {
+        size8 memory = 0;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+        
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, present)
+    {
+        size8 memory = 1;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_TRUE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        ASSERT_TRUE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, writable)
+    {
+        size8 memory = 1 << 1;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_TRUE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_TRUE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, user)
+    {
+        size8 memory = 1 << 2;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_TRUE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_TRUE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, write_through)
+    {
+        size8 memory = 1 << 3;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_TRUE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_TRUE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, cache)
+    {
+        size8 memory = 1 << 4;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_TRUE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_TRUE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, accessed)
+    {
+        size8 memory = 1 << 5;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_TRUE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_TRUE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, dirty)
+    {
+        size8 memory = 1 << 6;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_TRUE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_TRUE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, global)
+    {
+        size8 memory = 1 << 8;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_TRUE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_TRUE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, available)
+    {
+        size8 memory = 3 << 9;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(3,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(3,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, attribute)
+    {
+        size8 memory = 1 << 12;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(1,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(1,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, address)
+    {
+        size8 memory = 0xFFFFFFFE00000;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0xFFFFFFFE00000,reference.address());
+        ASSERT_FALSE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x7FFFFFFF, 0 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0xFFFFFFFE00000,fields.address());
+        ASSERT_FALSE(fields.executable());
+    }
+
+    TEST(long_large_page_directory_entry, executable)
+    {
+        size8 memory = size8{1} << 63;
+        auto& reference = reinterpret_cast<long_large_page_directory_entry&>(memory);
+        ASSERT_FALSE(reference.present());
+        ASSERT_FALSE(reference.writable());
+        ASSERT_FALSE(reference.user());
+        ASSERT_FALSE(reference.write_through());
+        ASSERT_FALSE(reference.cache());
+        ASSERT_FALSE(reference.accessed());
+        ASSERT_FALSE(reference.dirty());
+        ASSERT_FALSE(reference.global());
+        ASSERT_EQ(0,reference.available());
+        ASSERT_EQ(0,reference.attribute());
+        ASSERT_EQ(0,reference.address());
+        ASSERT_TRUE(reference.executable());
+
+        auto fields = long_large_page_directory_entry { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+        ASSERT_FALSE(fields.present());
+        ASSERT_FALSE(fields.writable());
+        ASSERT_FALSE(fields.user());
+        ASSERT_FALSE(fields.write_through());
+        ASSERT_FALSE(fields.cache());
+        ASSERT_FALSE(fields.accessed());
+        ASSERT_FALSE(fields.dirty());
+        ASSERT_FALSE(fields.global());
+        ASSERT_EQ(0,fields.available());
+        ASSERT_EQ(0,fields.attribute());
+        ASSERT_EQ(0,fields.address());
+        ASSERT_TRUE(fields.executable());
     }
 }
