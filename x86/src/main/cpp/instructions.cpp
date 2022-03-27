05c07f211b987f4f1968414be4c991af43b3494b
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-03-27 01:09:46 -0300

    Move rdmsr and wrmsr.

--- x86/src/main/cpp/instructions.cpp
+++ x86/src/main/cpp/instructions.cpp
@@ -8,9 +8,15 @@
 namespace x86
 {
     // #XXX: LLVM 13 doesn't know how to do extended asm with _ExtInt
-    struct carrier { size data {}; };
 
+    struct carrier { size data {}; };
     static_assert(sizeof(carrier) == sizeof(size), "unexpected size of carrier");
+
+    struct carrier4 { size4 data {}; };
+    static_assert(sizeof(carrier4) == sizeof(size4), "unexpected size of carrier4");
+
+    struct carrier8 { size8 data {}; };
+    static_assert(sizeof(carrier8) == sizeof(size8), "unexpected size of carrier8");
 
     auto cpuid2 (size feature, size variant) -> cpuid_type
     {
@@ -27,5 +33,20 @@ namespace x86
     void pause ()
     {
         __asm__ ( "pause" : );
+    }
+
+    auto rdmsr (size4 id) -> size8
+    {
+        carrier4 _id { id };
+        carrier8 _value {};
+        __asm__ ( "rdmsr " : "=A"(_value) : "c"(_id) : );
+        return _value.data;
+    }
+
+    void wrmsr (size4 id, size8  value)
+    {
+        carrier4 _id { id };
+        carrier8 _value { value };
+        __asm__ ( "wrmsr " : : "c"(_id), "A"(_value) : );
     }
 }
