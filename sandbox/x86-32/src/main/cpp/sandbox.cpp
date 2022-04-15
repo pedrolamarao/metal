660c2808a952c6619b2d4f694e0b2cdaa5488c72
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-04-14 23:28:44 -0300

    Rename port -> debugcon

--- sandbox/x86-32/src/main/cpp/sandbox.cpp
+++ sandbox/x86-32/src/main/cpp/sandbox.cpp
@@ -11,14 +11,14 @@ namespace psys
 
         // The machine is prepared for C++ automatic variables and procedure calls.
 
-        const short port { 0xE9 };
+        const unsigned short debugcon { 0xE9 };
 
         // GNU extended inline assembly default syntax is AT&T.
 
-        __asm__ ( "outb %0, %1" : : "a"('P'), "Nd"(port) : );
-        __asm__ ( "outb %0, %1" : : "a"('S'), "Nd"(port) : );
-        __asm__ ( "outb %0, %1" : : "a"('Y'), "Nd"(port) : );
-        __asm__ ( "outb %0, %1" : : "a"('S'), "Nd"(port) : );
+        __asm__ ( "outb %0, %1" : : "a"('P'), "Nd"(debugcon) : );
+        __asm__ ( "outb %0, %1" : : "a"('S'), "Nd"(debugcon) : );
+        __asm__ ( "outb %0, %1" : : "a"('Y'), "Nd"(debugcon) : );
+        __asm__ ( "outb %0, %1" : : "a"('S'), "Nd"(debugcon) : );
 
         // Microsoft assembly block default syntax is Intel.
 
