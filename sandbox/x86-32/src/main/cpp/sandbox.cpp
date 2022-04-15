3b81df6e09270bbdb9b6d17cf7c564a463b1b180
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-04-14 23:24:43 -0300

    Exemplify inline assembly.

--- sandbox/x86-32/src/main/cpp/sandbox.cpp
+++ sandbox/x86-32/src/main/cpp/sandbox.cpp
@@ -1,7 +1,5 @@
 // Copyright (C) 2022 Pedro Lamarão <pedro.lamarao@gmail.com>. All rights reserved.
 
-#include <x86/ports.h>
-
 
 //! Play here!
 
@@ -9,13 +7,33 @@ namespace psys
 {
     void sandbox ()
     {
-        // Look for this output in QEMU debugcon!
+        // Look for output in QEMU debugcon!
 
-        auto debug = x86::port<1>(0xE9);
-        debug.write('P');
-        debug.write('S');
-        debug.write('Y');
-        debug.write('S');
+        // The machine is prepared for C++ automatic variables and procedure calls.
+
+        const short port { 0xE9 };
+
+        // GNU extended inline assembly default syntax is AT&T.
+
+        __asm__ ( "outb %0, %1" : : "a"('P'), "Nd"(port) : );
+        __asm__ ( "outb %0, %1" : : "a"('S'), "Nd"(port) : );
+        __asm__ ( "outb %0, %1" : : "a"('Y'), "Nd"(port) : );
+        __asm__ ( "outb %0, %1" : : "a"('S'), "Nd"(port) : );
+
+        // Microsoft assembly block default syntax is Intel.
+
+        __asm__ {
+            push eax
+            mov al, 'P'
+            out 0xE9, al
+            mov al, 'S'
+            out 0xE9, al
+            mov al, 'Y'
+            out 0xE9, al
+            mov al, 'S'
+            out 0xE9, al
+            pop eax
+        }
     }
 }
 
