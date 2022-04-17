77b1d01be6cd31fdd28d1c16c34cdb31c4b9b3b6
Pedro Lamarão <pedro.lamarao@gmail.com>
2022-04-17 13:00:43 -0300

    Define for x86_64-multiboot2-elf.

--- multiboot2/start/src/main/cpp/start.cpp
+++ multiboot2/start/src/main/cpp/start.cpp
@@ -4,6 +4,9 @@
 #include <multiboot2/header.h>
 #include <multiboot2/start.h>
 
+#include <x86/pages.h>
+#include <x86/segments.h>
+
 
 namespace multiboot2
 {
@@ -29,34 +32,46 @@ namespace multiboot2
     constinit
     unsigned char stack [ 0x4000 ] {};
 
-    //! Start procedure.
+    //! x86_32 start procedure.
 
-    extern "C"
-    [[gnu::naked, gnu::section(".multiboot2.start"), gnu::used]]
-    void multiboot2_start ()
+    void start_x86_32 ();
+
+    //! x86-64 start procedure.
+
+    void start_x86_64 ();
+}
+
+//! Start procedure.
+
+extern "C"
+[[gnu::naked, gnu::section(".multiboot2.start"), gnu::used]]
+void multiboot2_start ()
+{
+    __asm__
     {
+        .code32
+        // set stack
+        mov esp, offset multiboot2::stack + 0x4000
+        mov ebp, esp
+        // reset eflags
+        xor ecx, ecx
+        push ecx
+        popfd
+        // #TODO: validate magic number in eax
+    }
 #if defined(__i386__)
-        __asm__
-        {
-            // set stack
-            mov esp, offset stack + 0x4000
-            mov ebp, esp
-            // reset eflags
-            xor ecx, ecx
-            push ecx
-            popfd
-            // call C++
-            push ebx
-            push eax
-            call main
-            // finish
-            cli
-          halt:
-            hlt
-            jmp halt
-        }
+    __asm__
+    {
+        .code32
+        jmp multiboot2::start_x86_32
+    }
+#elif defined(__x86_64__)
+    __asm__
+    {
+        .code32
+        jmp multiboot2::start_x86_64
+    }
 #else
 # error unsupported target
 #endif
-    }
 }
