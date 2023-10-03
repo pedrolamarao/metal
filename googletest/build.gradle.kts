394cb14544b9562371c400bc123ef94537d602ea
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-10-03 18:43:23 -0300

    Provide prebuilt googletest from source.
    Adds a dependency on cmake and ninja.

--- googletest/build.gradle.kts
+++ googletest/build.gradle.kts
@@ -1,11 +1,41 @@
+import org.ajoberstar.grgit.Grgit
+
 plugins {
     id("br.dev.pedrolamarao.metal.cpp")
     id("br.dev.pedrolamarao.metal.prebuilt")
+    id("org.ajoberstar.grgit") version("5.2.0") apply(false)
+}
+
+val source = layout.buildDirectory.dir("src").get()
+val build = layout.buildDirectory.dir("obj").get()
+
+val clone = tasks.register("clone") {
+    doLast {
+        if (! source.asFile.exists()) {
+            Grgit.clone {
+                depth = 1
+                dir = source
+                uri = "https://github.com/google/googletest"
+            }
+        }
+    }
+}
+
+val configure = tasks.register<Exec>("configure") {
+    dependsOn(clone)
+    executable("cmake")
+    args("-B",build,"-DCMAKE_BUILD_TYPE=Release","-G","Ninja","-S",source)
+}
+
+val make = tasks.register<Exec>("make") {
+    dependsOn(configure)
+    executable("cmake")
+    args("--build",build)
 }
 
 metal {
     prebuilt {
-        includable( layout.projectDirectory.dir("src/googletest/include") )
-        linkable( layout.projectDirectory.file("src/lib/Release/gtest.lib") )
+        includable(source.dir("googletest/include")) { builtBy(clone) }
+        linkable(build.file("lib/gtest.lib")) { builtBy(make) }
     }
 }
