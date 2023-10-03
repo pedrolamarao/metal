c3c39f4fddb3b5d9c91f4c8d8958e083f4377e4d
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-10-03 14:06:05 -0300

    Set Gradle Metal version to 0.1-rc-0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.startsWith("br.dev.pedrolamarao.metal.")) {
-                useModule("br.dev.pedrolamarao.gradle.metal:plugins:[0.1,0.2)")
+                useModule("br.dev.pedrolamarao.gradle.metal:plugins:0.1-rc-0")
             }
         }
     }
