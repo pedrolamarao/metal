2218489659cd75de256b24b807384fe4363e2bd2
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-10-05 19:39:48 -0300

    Upgrade Gradle Metal to 0.2 (#152)
    * Update Gradle Metal to 0.2.
    
    * Remove plugin repository mavenLocal.

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -6,7 +6,7 @@ group = "br.dev.pedrolamarao.metal.plugins"
 
 val elf = "1.0.0-SNAPSHOT"
 val gdb = "1.0.0-SNAPSHOT"
-val metal = "0.1-rc-1"
+val metal = "0.2"
 
 kotlin.jvmToolchain {
     languageVersion.set(JavaLanguageVersion.of(17))
