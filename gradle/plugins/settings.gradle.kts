835472c90f9072520a778c565d10b284ca07a26e
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-10-05 19:30:39 -0300

    Remove plugin repository mavenLocal.

--- gradle/plugins/settings.gradle.kts
+++ gradle/plugins/settings.gradle.kts
@@ -6,6 +6,5 @@ dependencyResolutionManagement {
             name = "Sonatype Open Source Snapshot Repository"
             url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
         }
-        mavenLocal()
     }
 }
